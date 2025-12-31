e6169111a2d4e8edecbda112f735487a3bae1a16
Todd Tanner <todd@spawndev.com>
2025-12-30 23:36:22 -0500

    Add project files.

---
+++ WorkerTransferExample/Program.cs
@@ -0,0 +1,21 @@
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using SpawnDev;
+using SpawnDev.BlazorJS;
+using SpawnDev.BlazorJS.WebWorkers;
+using WorkerTransferExample;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.Services.AddBlazorJSRuntime(out var JS);
+builder.Services.AddWebWorkerService(webWorkerService =>
+{
+    // warm up a single worker in the pool
+    webWorkerService.TaskPool.PoolSize = 1;
+});
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+if (JS.IsWindow)
+{
+    builder.RootComponents.Add<App>("#app");
+    builder.RootComponents.Add<HeadOutlet>("head::after");
+}
+await builder.Build().BlazorJSRunAsync();
